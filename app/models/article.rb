6f35fe08bb8c8d121ca2d14ee1884267cb7716ab
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-16 00:46:55 +0900

    記事を作成するとき中間テーブルにもデータを作成する。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  has_many :categories, through: :category_articles
   has_many :category_articles, dependent: :destroy
+  has_many :categories, through: :category_articles
   accepts_nested_attributes_for :category_articles
 
   validates :title, presence: true, length: { maximum: 50 }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :articles, through: :category_articles
   has_many :category_articles, dependent: :destroy
+  has_many :articles, through: :category_articles
 
   validates :name, presence: true, length: { maximum: 20 }
 end
