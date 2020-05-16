7882958266e31e1e95818b46b34762b05824ed7b
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-16 11:31:00 +0900

    userのvalidation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+       
+  validates :nickname,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 20 }     
+  validates :email,
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: true,
+    length: { maximum: 50 }
+  validates :password,
+    format: { with: VALID_PASSWORD_REGEX }
 end
