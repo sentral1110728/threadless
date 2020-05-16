359b3f38adb6b9ba60a125a1ac22593615623cb2
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-16 10:57:06 +0900

    記事の閲覧以外はログイン必須にする(ログイン画面へ遷移させる)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
   before_action :set_article, only: %i[show edit update destroy]
 
   # GET /articles
